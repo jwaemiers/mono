1dafe8ffcacf4b776078778910c6aaed26ca3da8
jwaemiers <james333weir@gmail.com>
2026-02-05 19:54:54 +1000

    src(truss): create TrussSolver class

--- projects/truss_solver/src/truss_solver.py
+++ projects/truss_solver/src/truss_solver.py
@@ -1,5 +1,9 @@
+from typing import TypeAlias
 from truss import TrussBuilder
 import numpy as np
+from numpy.typing import NDArray
+
+NDArray1D: TypeAlias = NDArray[np.float64]
 
 
 class TrussSolver:
